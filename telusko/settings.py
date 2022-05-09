e162234bf0177a20c0ed7a7347f46e8f9182082e
Dhruv <dhruvpatel19110@gmail.com>
2022-05-09 12:21:32 +0530

    add dptractors

--- telusko/settings.py
+++ telusko/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from msilib.schema import Media
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,10 +129,11 @@ MEDIA_URL= '/media/'
 
 MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+django_heroku.settings(locals())
