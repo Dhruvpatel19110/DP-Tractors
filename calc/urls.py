bd96597e3abaf5bc3bcb3fb5081997cd4a884e7b
Dhruv <dhruvpatel19110@gmail.com>
2022-06-15 20:31:06 +0530

    message

--- calc/urls.py
+++ calc/urls.py
@@ -28,5 +28,3 @@ urlpatterns = [
     path("addgallery/", views.Add_Gallery, name='addgallery'),
 
 ]
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
